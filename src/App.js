df5fed2a4c447d76ed1e37f90f407c83f8a0b3ad
sandy <sandy.rav@onja.org>
2021-08-25 15:48:39 +0300

    creating react app to practice writing tailwind css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Profile from './images/pic.jpg'
+import Image1 from './images/image1.jpeg'
+import Image2 from './images/image2.jpeg'
+import Image3 from './images/image3.jpeg'
+import Image4 from './images/image4.jpeg'
+import Image5 from './images/image5.jpeg'
+import Image6 from './images/image6.jpeg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='App' class='px-8 py-8'>
+      <header class='text-center'>
+        <img class='w-48 rounded-full m-auto' src={Profile} alt='Profile' />
+        <h1 class='text-red-600 px-4 py-4'>Sandy Rav</h1>
+        <p class='text-white-400 px-4 py-4'>
+          Hello, I’m Sandy. I'm a web developer at Onja. You can contact me via
+          email.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div class='text-center'>
+        <p>
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum. Why do we use it?
+        </p>
+      </div>
+      <div class='grid grid-cols-3 grid-rows-2 p-8'>
+        <img src={Image1} class='w-64 h-48 object-fit gap-4' alt='Shooting' />
+        <img src={Image2} class='w-64 h-48 object-fit gap-4' alt='Shooting' />
+        <img src={Image3} class='w-64 h-48 object-fit gap-4' alt='Shooting' />
+        <img src={Image4} class='w-64 h-48 object-fit gap-4' alt='Shooting' />
+        <img src={Image5} class='w-64 h-48 object-fit gap-4' alt='Shooting' />
+        <img src={Image6} class='w-64 h-48 object-fit gap-4' alt='Shooting' />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
